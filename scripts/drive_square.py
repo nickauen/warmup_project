7a7732fa8a4e37aec58fca8790bbfc5c2db9b800
nickauen <nickauen@mac.com>
2022-04-05 13:37:28 +0000

    Adding comments

--- scripts/drive_square.py
+++ scripts/drive_square.py
@@ -9,29 +9,31 @@ class DriveSquare(object):
     """ This node published ROS messages to make a turtlebot drive in a square """
 
     def __init__(self):
+        # Initiates the drive_square node
         rospy.init_node('drive_square')
-    
+        # Initiates the publisher for drive_square (publishes Twist)
         self.drive_square_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-
+        # Sleep to ensure node connections are established
         rospy.sleep(1)
 
     def run(self):
-        r = rospy.Rate(5.0)
+        r = rospy.Rate(5.0) # 5Hz
         while not rospy.is_shutdown():
             cmd = Twist()
-            cmd.linear.x = 0.21
-            for i in range(5):
+            cmd.linear.x = 0.21 # Sets forward movement vector
+            for i in range(5): # Iterates 5 times
                 self.drive_square_pub.publish(cmd)
                 rospy.sleep(1)
 
             cmd = Twist()
-            cmd.angular.z = 0.315
-            for i in range(5):
+            cmd.angular.z = 0.315 # Sets angular (spinning) movement vector
+            for i in range(5): # Iterates 5 times
                 self.drive_square_pub.publish(cmd)
                 rospy.sleep(1)
 
             
 
 if __name__ == '__main__':
+    # Run the node
         node = DriveSquare()
         node.run()
