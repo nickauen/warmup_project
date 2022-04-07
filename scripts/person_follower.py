28af4a9a35545072dd6ded8265f03fdbdf6ee9bd
nickauen <nickauen@mac.com>
2022-04-07 20:45:42 +0000

    updated person_follower.py

--- scripts/person_follower.py
+++ scripts/person_follower.py
@@ -10,6 +10,7 @@ import numpy
 from geometry_msgs.msg import Twist, Vector3
 from sensor_msgs.msg import LaserScan
 
+
 class Follow(object):
     """ This node published ROS messages to make a turtlebot follow the closest thing to it """
 
@@ -22,42 +23,50 @@ class Follow(object):
         
 
         rospy.sleep(1)
-
+    
     def process_scan(self, data):
         goal_orientation = 0
-        angular_constant = .01
-        goal_distance = 0.5
+        angular_constant = 0.8
+        goal_distance = 0.3
         angular_speed = 0
         linear_speed = 0
+        linear_constant = 0.5
         cmd = Twist()
 
-
-        low_value = None
+ 
         
+        low_value = 50
         index = 0
-        for x in data.ranges:
-            index += 1
-            if (low_value == None or x < low_value):
-                low_value = x
-                
-                current_orientation = index
-        
-        distance = (data.ranges[355] + data.ranges[356] + data.ranges[357] + data.ranges[358] + data.ranges[359] + 
-        data.ranges[0] + data.ranges[1] + data.ranges[2] + data.ranges[3] + data.ranges[4])/10
+        for x in range(359):
+            if ((data.ranges[x] != 0.0) and (data.ranges[x] < low_value)):
+                low_value = data.ranges[x]
+               
+                current_orientation = x
         
         
-        if ((current_orientation > 0) and (current_orientation <= 180)):
-            angular_speed = angular_constant*current_orientation/2
-            linear_speed = 0.25
+        distance = (data.ranges[351] + data.ranges[352] + data.ranges[353] + data.ranges[354] + data.ranges[355] + data.ranges[356] + data.ranges[357] + data.ranges[358] + data.ranges[359] + 
+        data.ranges[0] + data.ranges[1] + data.ranges[2] + data.ranges[3] + data.ranges[4] + data.ranges[5] + data.ranges[6] + data.ranges[7] + data.ranges[8] + data.ranges[9])/19
+        
+        """if ((current_orientation < 25) or (current_orientation > 330)):
+            angular_speed = 0.0
+            linear_speed = linear_constant*distance"""
 
-        if ((current_orientation < 359) and (current_orientation > 180)):
-            angular_speed = angular_constant*current_orientation/2*-1
-            linear_speed = 0.25
+        linear_speed = linear_constant*distance/2
+        if ((current_orientation <= 25) or (current_orientation >= 340)):
+            linear_speed = linear_constant*distance
 
-        if ((current_orientation > 90) and (current_orientation < 270)):
-            linear_speed = 0.0
+        if ((current_orientation > 10) and (current_orientation <= 180)):
+            angular_speed = angular_constant*(current_orientation/2)
+          
+
+        if ((current_orientation < 350) and (current_orientation > 180)):
+            angular_speed = -1*(angular_constant*(1/current_orientation))*500
+            
+
+        """if ((current_orientation > 90) and (current_orientation < 270)):
+            linear_speed = 0.0"""
         
-        if((distance > 9) or (distance <= goal_distance)):
+        if(distance <= goal_distance):
             linear_speed = 0.0
 
         cmd.linear.x = linear_speed
